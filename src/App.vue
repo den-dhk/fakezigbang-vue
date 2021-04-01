481492ecf8b4261a0fd6f1bf2fb602cf993a78b7
Deokhwa <chchch014@gmail.com>
2021-04-01 13:59:08 +0900

    lec 2

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,34 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <div>
-      <h4>XX 원룸</h4>
-      <p>XX 만원</p>
+    fake-zigbang
+      <h4 class="red" :style="스타일">{{ products[1] }}</h4>
+      <p> {{ price1 }} 만원</p>
   </div>
   <div>
-      <h4>XX 원룸</h4>
-      <p>XX 만원</p>
-  </div>
+      <h4>{{ products[1] }}</h4>
+      <p> {{ price2 }} 만원</p>
+  </div> 
+  <div>
+      <h4>{{ products[2] }}</h4>
+      <p> {{ price3 }} 만원</p>
+  </div> 
+
 </template>
 
 <script>
-
+ 
 export default {
   name: 'App',
+  data(){                       //데이터 보관함
+    return {
+      price1 : 60,
+      price2 : 70,
+      price3 : 80,
+      스타일 : 'color : blue',
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    }
+  },
   components: {
    
   }
